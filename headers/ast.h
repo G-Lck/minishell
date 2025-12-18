964e48082abba23df806e5275e162abcc9d89d17
G-Lck <gje.lucken@gmail.com>
2025-12-18 11:01:50 +0100

    new ast and new print ast

--- headers/ast.h
+++ headers/ast.h
@@ -23,18 +23,18 @@ typedef struct s_ast
 {
 	t_node_type	node_type;
 	t_list	*lst_token;
-	t_list	*next_left;
-	t_list	*next_right;
+	struct s_ast	*next_left;
+	struct s_ast	*next_right;
 	t_list	*redirs;
 	int		lst_len;
 	int		exec_status;
+	bool	is_subshell;
 } t_ast;
 
-void	ft_astadd_left(t_ast **ast, t_ast *new);
-void	ft_astadd_right(t_ast **ast, t_ast *new);
 t_ast	*ft_astnew(t_list *lst_token, int len);
 void	create_ast(t_ast *node);
 void	test_ast(t_minishell *minishell);
 void	print_ast(t_ast *node);
+void	print_ast_pretty(t_ast *node);
 
 # endif
--- tests/main-garance.c
+++ tests/main-garance.c
@@ -3,16 +3,16 @@
 int	main(void)
 {
 	t_minishell	minishell;
-	t_ast		*node; 		
+	t_ast		*node;
 
 	minishell.tokens_list = NULL;
 
-	tokenizer("a&& ((b && c))", &minishell);
+	tokenizer("(a b&& b | c | (d&&g) | e || f)", &minishell);
 	syntax_checker(&minishell);
 	print_token(&minishell);
 	minishell.ast = ft_astnew(minishell.tokens_list, ft_lstsize(minishell.tokens_list));
 	create_ast(minishell.ast);
-	ft_printf("\n AST\n\n");
-	print_ast(minishell.ast);
+	ft_printf("\n AST (OMG)\n\n");
+	print_ast_pretty(minishell.ast);
 	free_token_list(&minishell.tokens_list);
 }
