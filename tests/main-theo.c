4859cdeb234674d03445d1f2ee1863dad540da96
Theo Bouverat <theo@macbookpro.home>
2025-12-20 12:39:53 +0100

    up

--- tests/main-theo.c
+++ tests/main-theo.c
@@ -1,42 +1,43 @@
 #include "minishell.h"
 
-// void	ast_descent(t_test *node)
-// {
-// 	if (node->node_type == AND_OP || node->node_type == OR_OP || node->node_type == PIPE_OP)
-// 		ast_descent (node->left);
-// 	else
-// 	{
-// 		ft_printf("(%s)", node->literal);
-// 		return ;
-// 	}
+void	ast_descent(t_ast *node)
+{
+	if (node->node_type == AND_OP || node->node_type == OR_OP || node->node_type == PIPE_OP)
+		ast_descent (node->next_left);
+	else
+	{
+		t_token *token = node->lst_token->content;
+		ft_printf("(%s)", token->literal);
+		return ;
+	}
 
-// 	if (node->node_type == AND_OP)
-// 	{
-// 		if (node->left->exec_status == 0)
-// 		{
-// 			ast_descent(node->right);
-// 			node->exec_status = node->right->exec_status;
-// 		}
-// 		else
-// 			node->exec_status = node->left->exec_status;
-// 	}
-// 	else if (node->node_type == OR_OP)
-// 	{
-// 		if (node->left->exec_status != 0)
-// 		{
-// 			ast_descent(node->right);
-// 			node->exec_status = node->right->exec_status;
-// 		}
-// 		else
-// 			node->exec_status = node->left->exec_status;
-// 	}
-// 	else if (node->node_type == PIPE_OP)
-// 	{
-// 		ast_descent(node->right);
-// 		node->exec_status = node->left->exec_status;
-// 	}
-// 	return ;
-// }
+	if (node->node_type == AND_OP)
+	{
+		if (node->next_left->exec_status == 0)
+		{
+			ast_descent(node->next_right);
+			node->exec_status = node->next_right->exec_status;
+		}
+		else
+			node->exec_status = node->next_left->exec_status;
+	}
+	else if (node->node_type == OR_OP)
+	{
+		if (node->next_left->exec_status != 0)
+		{
+			ast_descent(node->next_right);
+			node->exec_status = node->next_right->exec_status;
+		}
+		else
+			node->exec_status = node->next_left->exec_status;
+	}
+	else if (node->node_type == PIPE_OP)
+	{
+		ast_descent(node->next_right);
+		node->exec_status = node->next_left->exec_status;
+	}
+	return ;
+}
 
 int	main(int argc, char *argv[])
 {
@@ -50,38 +51,13 @@ int	main(int argc, char *argv[])
 		minishell.input = readline("\e[0;36m\nMinihell > \e[0;33m");
 		tokenizer(minishell.input, &minishell);
 		if (syntax_checker(&minishell))
-			print_token(&minishell);
+		{
+			minishell.ast = ft_astnew(minishell.tokens_list, ft_lstsize(minishell.tokens_list));
+			create_ast(minishell.ast);
+			print_ast_pretty(minishell.ast);
+		}
+		free_ast(minishell.ast);
 		free_token_list(&minishell.tokens_list);
 	}
 	return (0);
-
-	// t_test	head;
-	// t_test second_level_left;
-	// t_test second_level_right;
-	// t_test third_level_left;
-	// t_test third_level_right;
-
-	// head.node_type = PIPE_OP;
-	// second_level_left.node_type = AND_OP;
-	// second_level_right.node_type = CMD;
-	// third_level_left.node_type = CMD;
-	// third_level_right.node_type = CMD;
-
-	// head.literal = "||";
-	// second_level_right.literal = "echo c";
-	// third_level_left.literal = "echo a";
-	// third_level_right.literal = "echo b";
-
-	// head.exec_status = 0;
-	// second_level_left.exec_status = 0;
-	// third_level_left.exec_status = 1;
-	// third_level_right.exec_status = 0;
-	// second_level_right.exec_status = 0;
-
-	// head.left = &second_level_left;
-	// head.right = &second_level_right;
-	// second_level_left.left = &third_level_left;
-	// second_level_left.right = &third_level_right;
-
-	// ast_descent(&head);
 }
