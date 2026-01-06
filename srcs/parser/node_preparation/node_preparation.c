a3122c092bf8a7df339c79af8c82b7619b0c9bc2
Theo Bouverat <thbouver@c1r13s7.42lausanne.ch>
2026-01-06 14:19:44 +0100

    token parser

--- srcs/parser/node_preparation/node_preparation.c
+++ srcs/parser/node_preparation/node_preparation.c
@@ -6,7 +6,7 @@
 /*   By: thbouver <thbouver@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/06 18:35:07 by theo              #+#    #+#             */
-/*   Updated: 2026/01/05 17:45:09 by thbouver         ###   ########.fr       */
+/*   Updated: 2026/01/06 13:58:15 by thbouver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,27 @@ int	create_redir_node(char *target, t_token_type type, t_ast *node)
 	if (!new_node)
 		return (free(new_redir->target), free(new_redir), 0);
 	ft_lstadd_back(&node->redirs, new_node);
-	ft_printf("%d target : %s\n", type, target);
+	ft_printf("redir : %d -> %s\n", new_redir->redir_type, new_redir->target);
+	return (1);
+}
+
+int	create_command_node(t_token *token, t_ast *node)
+{
+	t_list	*new_node;
+	t_token	*new_token;
+
+	new_token = malloc(sizeof(t_token));
+	if (!new_token)
+		return (0);
+	new_token->literal = ft_strdup(token->literal);
+	if (!new_token->literal)
+		return (free(new_token),0);
+	new_token->type = token->type;
+	new_node = ft_lstnew(new_token);
+	if (!new_node)
+		return (free(new_token->literal), free(new_token), 0);
+	ft_lstadd_back(&node->lst_token, new_node);
+	ft_printf("token basic : %s\n",token->literal);
 	return (1);
 }
 
@@ -41,13 +61,16 @@ int	node_preparation(t_ast *node)
 	while (index < node->lst_len)
 	{
 		token = node->lst_token->content;
-		if (token->type == REDIR_IN || token->type == REDIR_OUT || token->type == HERE_DOC || token->type == APPEND)
+		if (token->type == REDIR_IN || token->type == REDIR_OUT
+			|| token->type == HERE_DOC || token->type == APPEND)
 		{
 			tmp = node->lst_token->next->content;
-			create_redir_nosde(tmp->literal, token->type, node);
+			create_redir_node(tmp->literal, token->type, node);
 			node->lst_token = node->lst_token->next;
 			index ++;
 		}
+		else
+			create_command_node(token, node);
 		node->lst_token = node->lst_token->next;
 		index ++;
 	}
